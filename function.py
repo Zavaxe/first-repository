33187d4c24e529af42f38e092db0f5da23784ed8
Zavaxe <zavax94@gmail.com>
2021-12-31 00:25:58 +0300

    Add function.py

---
+++ function.py
@@ -0,0 +1,10 @@
+def get_vat(payment, percent = 20):
+	try:
+		payment = float(payment)	
+		vat = payment / 100 * percent
+		vat = round(vat, 2)
+		return 'Sum VAT:{}'.format(vat)
+	except TypeError:
+		return "Can't count. check data!"
+result = get_vat(400)
+print(result)
